95ec1eb3a0681003365d7d32b263a32c1471bee1
Nimsara Lekamge <nimsaralekamge@gmail.com>
2026-05-09 20:49:22 +0530

    Revert "part 3  (proxy)"
    This reverts commit 5ab758476522e35ed77af99b3b408fcdb5c8dada.

--- TechTitans/src/main/java/com/example/techtitans/Repository/ProxyRepository.java
+++ TechTitans/src/main/java/com/example/techtitans/Repository/ProxyRepository.java
@@ -1,10 +1,4 @@
 package com.example.techtitans.Repository;
 
-import com.example.techtitans.Entity.Proxy;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProxyRepository extends JpaRepository<Proxy, String> {
-    //  "Proxy" means your Entity class, "String" means Primary Key (proxyId) type
+public interface ProxyRepository {
 }
