f706fa405293f21274959b08c520f7c26402b8e4
Nimsara Lekamge <nimsaralekamge@gmail.com>
2026-05-09 20:43:48 +0530

    Revert "database"
    This reverts commit d5fae4ade8d58383c17fcb05ea3c3ac5867c50a5.

--- TechTitans/src/main/java/com/example/techtitans/Repository/ProxyRepository.java
+++ TechTitans/src/main/java/com/example/techtitans/Repository/ProxyRepository.java
@@ -1,8 +1,5 @@
 package com.example.techtitans.Repository;
-import com.example.techtitans.Entity.Proxy;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-<<<<<<< Updated upstream
 import com.example.techtitans.Entity.Proxy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,6 +8,3 @@ import org.springframework.stereotype.Repository;
 public interface ProxyRepository extends JpaRepository<Proxy, String> {
     //  "Proxy" means your Entity class, "String" means Primary Key (proxyId) type
 }
-=======
-public interface ProxyRepository extends JpaRepository<Proxy, String> {}
->>>>>>> Stashed changes
