68563b0c3c78bfc93ff12d3bb92388dc2c2e7c55
Malindu Sahanpriya <malindusahanpriyasuraweera@gmail.com>
2026-05-09 17:46:14 +0530

    File structure

---
+++ TechTitans/src/main/java/com/example/techtitans/TechTitansApplication.java
@@ -0,0 +1,13 @@
+package com.example.techtitans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTitansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechTitansApplication.class, args);
+    }
+
+}
