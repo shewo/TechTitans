419e78eaecc4269fa93aff9ee4fcd5dc9c11fdf6
Malindu Sahanpriya <157407216+shewo@users.noreply.github.com>
2026-05-10 10:06:44 +0530

    Delete TechTitans directory

--- TechTitans/TechTitans/src/main/java/com/example/techtitans/Repository/AlertRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.techtitans.Repository;
-import com.example.techtitans.Entity.Alert;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlertRepository extends JpaRepository<Alert, String> {}
